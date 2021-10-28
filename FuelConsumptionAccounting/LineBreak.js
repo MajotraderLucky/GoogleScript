f09a5a2f4b7a91e0e66c7094de3772a9a5291fab
Sergey <majo78@mail.ru>
2021-10-28 21:37:51 +0300

    Add file LineBreak.js

---
+++ FuelConsumptionAccounting/LineBreak.js
@@ -0,0 +1,29 @@
+function lineBreak() {
+    var ss = SpreadsheetApp.openById('1swo1Ov08m9PNAZt72j3hhqTQIL6tlhi6qxWI3mLNRw4');
+    var activeSheet = ss.getActiveSheet();
+
+    if (activeSheet.getRange('G4').isChecked()) {
+
+        let numberLine = activeSheet.getRange('G6').getValue();//Номер строки, куда будут вноситься данные
+
+        if (numberLine !== 6) {
+
+            let date = activeSheet.getRange('A2').getValue();
+            activeSheet.getRange('A' + numberLine).setValue(date);
+            activeSheet.getRange('A' + numberLine + ':' + 'F' + numberLine).setHorizontalAlignment('center');
+            let departureKm = activeSheet.getRange('B2').getValue();
+            activeSheet.getRange('B' + numberLine).setValue(departureKm);
+            let arrivalKm = activeSheet.getRange('C2').getValue();
+            activeSheet.getRange('C' + numberLine).setValue(arrivalKm);
+            let fueled = activeSheet.getRange('D2').getValue();
+            activeSheet.getRange('D' + numberLine).setValue(fueled);
+            let restLastMonth = activeSheet.getRange('E2').getValue();
+            activeSheet.getRange('E' + numberLine).setValue(restLastMonth);
+            let newRest = activeSheet.getRange('F2').getValue();
+            activeSheet.getRange('F' + numberLine).setValue(newRest);
+            activeSheet.getRange('A' + numberLine + ':' + 'F' + numberLine).setBackground('#e0dddd');
+        } else {
+            Browser.msgBox('Нельзя изменять базовую строку!');
+        }
+    }
+}
