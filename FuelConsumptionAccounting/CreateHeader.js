73552ef3fd644b613103183326089ee71658c695
Sergey <majo78@mail.ru>
2021-10-28 21:31:58 +0300

    Change file CreateHeader.js

--- FuelConsumptionAccounting/CreateHeader.js
+++ FuelConsumptionAccounting/CreateHeader.js
@@ -1,5 +1,5 @@
 function CreateHeaderForCalculator() {
-    var ss = SpreadsheetApp.openById('1swo1Ov08m9PNAZt72j3hhqTQIL6tlhi6qxWI3mLNRw4');
+    var ss = SpreadsheetApp.openById('Your ID from google sheets');
     var activeSheet = ss.getActiveSheet();
 
     //Создаём чекбокс запуска макроса 'CreateHeader'
